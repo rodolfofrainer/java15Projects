09ba23f4c4da7a9825a753c135f27958ae2d33c1
Rodolfo F <rodolfo_frainer@hotmail.com>
2024-02-10 12:11:20 +0100

    update Button.setBounds()

--- src/calculator/Button.java
+++ src/calculator/Button.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Button extends JButton{
     int gridSize;
@@ -9,12 +10,21 @@ public class Button extends JButton{
 
     public Button(String text, int gridSize, int buttonHeight, int[] position) {
         super(text);
+
         this.gridSize = gridSize;
         this.buttonHeight = buttonHeight*gridSize;
         this.position = position;
+
+        Font font = getFont();
+        setFont(new Font(font.getName(), font.getStyle(), 25));
+
+        int spaceBetweenButtons = this.gridSize+Calculator.PADDING;
+        if(this.position[0]!=0){
+        setBounds(this.position[0]*spaceBetweenButtons+Calculator.PADDING, this.position[1]*spaceBetweenButtons, this.gridSize, this.buttonHeight);
+        }else{
+            setBounds(5, this.position[1]*spaceBetweenButtons, this.gridSize, this.buttonHeight);
+        }
     }
 
-    public void setBounds(){
-        setBounds(this.position[0], this.position[1], this.gridSize, this.buttonHeight);
-    }
+
 }
