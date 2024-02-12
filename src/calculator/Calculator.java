58fa083be35c4fb2d98663dc871aece0bf61056b
Rodolfo F <rodolfo_frainer@hotmail.com>
2024-02-12 11:04:50 +0100

    add extra row of buttons

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -8,8 +8,8 @@ import java.util.List;
 public class Calculator {
     final static int PADDING = 5;
     final static int WIDTH = 425 + PADDING*2;
-    final static int HEIGHT = 550 + PADDING*2;
-    final static int gridSize = 100;
+    final static int HEIGHT = 650 + PADDING*3;
+    final static int GRID_SIZE = 100;
     public static void main(String[] args) {
 
         JFrame frame = createFrame();
@@ -30,18 +30,25 @@ public class Calculator {
     private static ArrayList<Button> populateButtons() {
         //Create array with app buttons
         ArrayList<Button> buttons = new ArrayList<>();
-        List<String> buttonsLabels = Arrays.asList("÷", "x","-","7","8","9","4","5","6","1","2","3","=","+");
+        List<String> buttonsLabels = Arrays.asList("÷", "x","-","7","8","9","4","5","6","1","2","3","=","+","+/-", "0",".");
 
+        int numRows = (int) Math.ceil((double) buttonsLabels.size() / 3);
         for (int i = 0; i < buttonsLabels.size(); i++) {
-            int row = i / 3; // Calculate the row index
-            int col = i % 3; // Calculate the column index
+            int row = i / 3;
+            int col = i % 3;
+
+            // Adjust the indices for special buttons
+            if (i >= buttonsLabels.size() - 3) {
+                row = numRows - 2;
+                col = i - (buttonsLabels.size() - 3);
+            }
 
             if (buttonsLabels.get(i).equals("=")) {
-                buttons.add(new Button("=", gridSize, 3, new int[]{3,2}));
+                buttons.add(new Button("=", GRID_SIZE, 4, new int[]{3,2}));
             } else if (buttonsLabels.get(i).equals("+")) {
-                buttons.add(new Button("+", gridSize, 1, new int[]{3,1}));
+                buttons.add(new Button("+", GRID_SIZE, 1, new int[]{3,1}));
             } else {
-                buttons.add(new Button(buttonsLabels.get(i), gridSize, 1, new int[]{col, row+1}));
+                buttons.add(new Button(buttonsLabels.get(i), GRID_SIZE, 1, new int[]{col, row+1}));
             }
         }
 
@@ -61,7 +68,7 @@ public class Calculator {
         //Create app white bar for input
         JTextArea textArea = new JTextArea();
         textArea.setEditable(false);
-        textArea.setBounds(PADDING, PADDING, WIDTH-PADDING*3, gridSize-PADDING);
+        textArea.setBounds(PADDING, PADDING, WIDTH-PADDING*3, GRID_SIZE -PADDING);
         return textArea;
     }
 }
