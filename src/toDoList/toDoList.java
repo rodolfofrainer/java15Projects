3030359bd692f6f008dd5221910b5b72bebd8580
Rodolfo F <rodolfo_frainer@hotmail.com>
2024-02-17 14:15:22 +0100

    toDoList initial commit

---
+++ src/toDoList/toDoList.java
@@ -0,0 +1,18 @@
+package toDoList;
+
+import javax.swing.*;
+
+public class toDoList {
+    public static void main(String[] args) {
+        JFrame frame = createFrame();
+    }
+
+
+    public static JFrame createFrame(){
+        JFrame frame = new JFrame("To-do List");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        return frame;
+    }
+
+}
