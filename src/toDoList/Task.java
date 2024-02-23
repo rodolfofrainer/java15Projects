b2a4e4a23c3a9fa3890fe5c6be7024afedb666de
Rodolfo F <rodolfo_frainer@hotmail.com>
2024-02-23 13:50:18 +0100

    create task class

--- src/toDoList/Task.java
+++ src/toDoList/Task.java
@@ -1,4 +1,38 @@
 package toDoList;
 
+import java.util.Date;
+
 public class Task {
+    int id;
+    String description;
+    Date dateCreated;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+
+    public Task(int id, String description){
+        Date dateCreated = new Date();
+    }
 }
