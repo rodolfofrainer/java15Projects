f56ffac165e5901150f4ff1da97a1d70fbe2712a
Rodolfo F <rodolfo_frainer@hotmail.com>
2024-02-24 11:21:44 +0100

    implement constructor

--- src/toDoList/Button.java
+++ src/toDoList/Button.java
@@ -6,10 +6,19 @@ import java.awt.*;
 public class Button extends JButton {
     int height;
     int length;
+    int x;
+    int y;
 
-    public Button(String text, int height, int length){
+    public Button(String text, int height, int length, int x, int y){
         super(text);
         this.height = height;
         this.length = length;
+        this.x = x;
+        this.y = y;
+
+        Font font = getFont();
+        setFont(new Font(font.getFontName(),font.getStyle(), 20));
+
+        setBounds(x,y, this.length, this.height);
     }
 }
