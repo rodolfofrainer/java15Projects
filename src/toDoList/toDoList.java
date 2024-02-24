52372aa13bdbb5ff7485fb945a363c44b2de7010
Rodolfo F <rodolfo_frainer@hotmail.com>
2024-02-24 11:21:31 +0100

    refactor code

--- src/toDoList/toDoList.java
+++ src/toDoList/toDoList.java
@@ -3,16 +3,21 @@ package toDoList;
 import javax.swing.*;
 
 public class toDoList {
+    static final int FRAME_WIDTH = 500;
+    static final int FRAME_HEIGHT = 500;
     public static void main(String[] args) {
         JFrame frame = createFrame();
 
+
+        Button addTask = new Button("Add a Task", 50, 150,(FRAME_WIDTH+100)/3, 0);
+        frame.add(addTask);
     }
 
 
     public static JFrame createFrame(){
         JFrame frame = new JFrame("To-do List");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500,500);
+        frame.setSize(FRAME_WIDTH,FRAME_HEIGHT);
         frame.setVisible(true);
         return frame;
     }
