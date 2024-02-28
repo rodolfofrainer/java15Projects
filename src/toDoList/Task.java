cc096ca81f4d89eb7caf5a14308cecc482cbdee5
Rodolfo F <rodolfo_frainer@hotmail.com>
2024-02-28 14:55:05 +0100

    refactor Task class

--- src/toDoList/Task.java
+++ src/toDoList/Task.java
@@ -1,32 +1,23 @@
 package toDoList;
-
 import java.util.Date;
-
 public class Task {
     int id;
     String description;
-    Date dateCreated;
-
+    long dateCreated;
 
     public int getId() {
         return id;
     }
-
-
     public String getDescription() {
         return description;
     }
-
-    public Date getDateCreated() {
+    public long getDateCreated() {
         return dateCreated;
     }
 
-
-
     public Task(int id, String description){
-
         this.id = id;
         this.description = description;
-        this.dateCreated = new Date();
+        this.dateCreated = new Date().getTime();
     }
 }
