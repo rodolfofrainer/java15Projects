2f5f0b0bc450994f07f2492b6e0263b661010078
Rodolfo F <rodolfo_frainer@hotmail.com>
2024-02-28 14:49:41 +0100

    refactor Task.java

--- src/toDoList/Task.java
+++ src/toDoList/Task.java
@@ -12,24 +12,15 @@ public class Task {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
 
     public String getDescription() {
         return description;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
     public Date getDateCreated() {
         return dateCreated;
     }
 
-    public void setDateCreated(Date dateCreated) {
-        this.dateCreated = dateCreated;
-    }
 
 
     public Task(int id, String description){
