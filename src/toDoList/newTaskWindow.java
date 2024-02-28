fa61808ca5d15b3666e7df5974c926dc01090d5f
Rodolfo F <rodolfo_frainer@hotmail.com>
2024-02-28 11:40:48 +0100

    add "add task button" to new task window

--- src/toDoList/newTaskWindow.java
+++ src/toDoList/newTaskWindow.java
@@ -1,19 +1,41 @@
 package toDoList;
 
 import javax.swing.*;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+import java.awt.*;
 
-public class newTaskWindow extends JFrame{
-    public void NewTaskWindow() {
+public class newTaskWindow extends JFrame {
+    public newTaskWindow() {
+        int width = 600;
+        int height = 150;
         setTitle("New Window");
-        setSize(300, 100);
+        setSize(width, height);
 
-        JTextArea label = new JTextArea();
+        JPanel panel = new JPanel(new BorderLayout());
+        JTextPane label = new JTextPane();
+        StyledDocument doc = label.getStyledDocument();
+        SimpleAttributeSet center = new SimpleAttributeSet();
+        StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
+        doc.setParagraphAttributes(0, doc.getLength(), center, false);
+        label.setFont(new Font(label.getFont().getName(), label.getFont().getStyle(), 20));
 
-        JPanel panel = new JPanel();
-        panel.add(label);
-        add(panel);
+        panel.add(label, BorderLayout.CENTER);
 
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
+
+        Button addTaskButton = new Button("Add task", width, width, width, height);
+        panel.add(addTaskButton, BorderLayout.SOUTH);
+
+        add(panel);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            newTaskWindow frame = new newTaskWindow();
+            frame.setVisible(true);
+        });
     }
 }
