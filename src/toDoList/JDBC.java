8d9ae7cf0789b5c65c31fb4f3f567d7b02ed9aae
Rodolfo F <rodolfo_frainer@hotmail.com>
2024-03-02 12:35:53 +0100

    start refactoring for db integration

--- src/toDoList/JDBC.java
+++ src/toDoList/JDBC.java
@@ -1,7 +1,6 @@
 package toDoList;
 
 import java.sql.Connection;
-import java.sql.Driver;
 import java.sql.DriverManager;
 
 public class JDBC {
@@ -11,9 +10,6 @@ public class JDBC {
         String password = config.getPassword();
         String url = "jdbc:mysql://localhost:3306/tasks";
         try {
-            // Load the MySQL JDBC driver
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            // Establish the connection
             Connection conn = DriverManager.getConnection(url, username, password);
             System.out.println("Connected to the database!");
         } catch (Exception e) {
--- src/toDoList/Task.java
+++ src/toDoList/Task.java
@@ -2,22 +2,30 @@ package toDoList;
 import java.util.Date;
 public class Task {
     private int id;
+    private boolean completed;
+    private String dateCreated;
     private String description;
-    private long dateCreated;
 
     public int getId() {
         return id;
     }
+    public boolean getCompleted() {
+        return completed;
+    }
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+    public String getDateCreated() {
+        return dateCreated;
+    }
     public String getDescription() {
         return description;
     }
-    public long getDateCreated() {
-        return dateCreated;
-    }
 
-    public Task(int id, String description){
+    public Task(int id, String description,String dateCreated){
         this.id = id;
+        this.completed = false;
         this.description = description;
-        this.dateCreated = new Date().getTime();
+        this.dateCreated = dateCreated;
     }
 }
