d8e8a2d31b032a7e0f740ffffef7a217f79a9169
Rodolfo F <rodolfo_frainer@hotmail.com>
2024-03-03 13:53:31 +0100

    refactor code

--- src/toDoList/toDoList.java
+++ src/toDoList/toDoList.java
@@ -2,32 +2,26 @@ package toDoList;
 
 import javax.swing.*;
 import java.awt.*;
-import java.sql.Connection;
-import java.util.Arrays;
-import java.util.List;
 
-import static toDoList.JDBC.connection;
-import static toDoList.JDBC.injectItem;
-import static toDoList.ReadFile.returnItems;
+import static toDoList.JDBC.readItems;
 
 public class toDoList {
     static final int FRAME_WIDTH = 500;
     static final int FRAME_HEIGHT = 500;
     static final int PADDING = 5;
     public static void main(String[] args) {
-        Connection conn = connection();
         // test item injection
-//        JDBC.injectItem(conn, 1, "test");
-        populateDB(conn,"listOfIdeas.txt");
+       // JDBC.injectItem(conn, 1, "test");
+//        JDBC.populateDB("listOfIdeas.txt");
+        readItems();
         JFrame frame = createFrame();
+
+        System.out.println(readItems());
 
         JPanel tasksPanel = new JPanel();
         tasksPanel.setLayout(new BoxLayout(tasksPanel, BoxLayout.Y_AXIS));
 
-
         frame.add(tasksPanel, BorderLayout.CENTER);
-
-
 
         Button addTaskButton = new Button("Add a Task", 50, 150,(FRAME_WIDTH-150)/2, 0);
         addTaskButton.addActionListener(e -> {
@@ -40,12 +34,7 @@ public class toDoList {
         frame.setVisible(true);
     }
 
-    private static void populateDB(Connection conn, String filename) {
-        List<String> items = returnItems(filename);
-        for (int i = 0; i < items.size(); i++) {
-            injectItem(conn, i, items.get(i));
-        }
-    }
+
 
 
     public static JFrame createFrame(){
