fa02e6caefc0117d707bbaed2df5fba4f64c1a0d
Rodolfo F <rodolfo_frainer@hotmail.com>
2024-03-05 15:09:09 +0100

    refactor code

--- src/toDoList/TaskPanel.java
+++ src/toDoList/TaskPanel.java
@@ -7,17 +7,14 @@ import static toDoList.toDoList.populateJPanel;
 
 public class TaskPanel extends JPanel {
     private final JCheckBox checkBox;
-    private final JButton button;
-    private final JPanel tasksPanel;
 
     public TaskPanel(Task task, JPanel tasksPanel) {
-        this.tasksPanel = tasksPanel;
 
         checkBox = new JCheckBox();
         JLabel descriptionLabel = new JLabel(task.getDescription());
         JLabel taskId = new JLabel(String.valueOf(task.getId()));
 
-        button = new JButton("X");
+        JButton button = new JButton("X");
         button.setPreferredSize(new Dimension(80, 20));
 
         setLayout(new FlowLayout(FlowLayout.LEFT));
