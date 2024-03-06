04e3691c576441a5fdaf2c3fc4fcb11a59137a9e
Rodolfo F <rodolfo_frainer@hotmail.com>
2024-03-06 10:26:30 +0100

    refactor TaskPanel

--- src/toDoList/TaskPanel.java
+++ src/toDoList/TaskPanel.java
@@ -9,23 +9,31 @@ public class TaskPanel extends JPanel {
     private final JCheckBox checkBox;
 
     public TaskPanel(Task task, JPanel tasksPanel) {
+        setLayout(new BorderLayout());
 
-        checkBox = new JCheckBox();
+        JPanel infoPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         JLabel descriptionLabel = new JLabel(task.getDescription());
+        checkBox = new JCheckBox();
+
         JLabel taskId = new JLabel(String.valueOf(task.getId()));
+        infoPanel.add(taskId);
+        infoPanel.add(checkBox);
+        infoPanel.add(descriptionLabel);
 
-        JButton button = new JButton("X");
-        button.setPreferredSize(new Dimension(80, 20));
 
-        setLayout(new FlowLayout(FlowLayout.LEFT));
-        add(taskId);
-        add(checkBox);
-        add(descriptionLabel);
-        add(button);
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+        JButton deleteButton = new JButton("X");
+        deleteButton.setPreferredSize(new Dimension(50, 20));
+
+        buttonPanel.add(deleteButton);
+
+        add(infoPanel, BorderLayout.CENTER);
+        add(buttonPanel, BorderLayout.EAST);
 
         setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
-        button.addActionListener(e -> {
+        deleteButton.addActionListener(e -> {
             JDBC.deleteSingleItem(task);
             tasksPanel.removeAll();
             populateJPanel(tasksPanel);
