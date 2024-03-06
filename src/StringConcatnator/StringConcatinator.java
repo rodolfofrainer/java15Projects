60d88f4ff52e43ea0b70aaf09dcb66e7deffaa35
Rodolfo F <rodolfo_frainer@hotmail.com>
2024-03-06 16:29:43 +0100

    create StringConcatinator

---
+++ src/StringConcatnator/StringConcatinator.java
@@ -0,0 +1,31 @@
+package StringConcatnator;
+
+import java.util.Scanner;
+
+public class StringConcatinator {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a first String:");
+        String first_string = scanner.nextLine().trim();
+        System.out.print("Enter a second String:");
+        String second_string = scanner.nextLine().trim();
+
+        System.out.println("The concatenated string is: " + first_string + second_string);
+        System.out.println("The first word in reverse is: "+ new StringBuilder(first_string).reverse());
+        System.out.println("The second word in reverse is: "+ new StringBuilder(second_string).reverse());
+        System.out.println("**************************************************");
+        Thread.sleep(2000);
+        System.out.println("The length of the first string is: "+ first_string.length());
+        System.out.println("The length of the second string is: "+ second_string.length());
+        System.out.println("**************************************************");
+        Thread.sleep(2000);
+        System.out.println("The first string in uppercase is: "+ first_string.toUpperCase());
+        System.out.println("The second string in uppercase is: "+ second_string.toUpperCase());
+        System.out.println("**************************************************");
+        Thread.sleep(2000);
+        System.out.println("The first string in lowercase is: "+ first_string.toLowerCase());
+        System.out.println("The second string in lowercase is: "+ second_string.toLowerCase());
+        System.out.println("**************************************************");
+        Thread.sleep(2000);
+    }
+}
